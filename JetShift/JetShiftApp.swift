3981e1fe68bf4710bc3069092bb3e13de2d1ebb0
robdg22 <56501481+robdg22@users.noreply.github.com>
2026-01-17 12:06:55 +0000

    Initial Commit

---
+++ JetShift/JetShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  JetShiftApp.swift
+//  JetShift
+//
+//  Created by Rob Graham on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct JetShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JetShiftTests/JetShiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  JetShiftTests.swift
+//  JetShiftTests
+//
+//  Created by Rob Graham on 17/01/2026.
+//
+
+import Testing
+@testable import JetShift
+
+struct JetShiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
